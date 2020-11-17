1a779f4d4ba8e1a164e9f6a0cdc59af51e7ad1fd
vu-luong <vubinhcht@gmail.com>
2020-11-17 15:29:30 +1000

    :zap: fix: index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
 import React from "react"
 import { navigate } from "gatsby"
+import Layout from "../components/layout/layout"
+import Container from "react-bootstrap"
 
 class IndexPage extends React.Component {
 
-  componentDidMount() {
-    navigate("/EDOC2021");
-  }
+  // componentDidMount() {
+  //   navigate("/EDOC2021");
+  // }
 
   render() {
-    return null
+    return (
+      <Layout>
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </Layout>
+    )
   }
 }
 
