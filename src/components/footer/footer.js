92745fc888906956eea995b2fe3b0867a6ef1821
vu-luong <vubinhcht@gmail.com>
2020-11-22 10:55:25 +1000

    :zap: add: font-awesome set up

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,23 +1,40 @@
 import React from "react"
 import { Link } from "gatsby"
-import FA from 'react-fontawesome'
+import { Row, Col } from "react-bootstrap"
+import "../../utils/font-awesome"
 
 const Footer = ({ author, title }) => {
   return (
     <div className="footer">
       <div className="container">
         <hr className="border-primary" />
-        <a href="https://www.gatsbyjs.com/" label="button" target="_blank" rel="noopener noreferrer"><FA name="twitter" /></a>
         <p>
-          {title}
+          <Row style={{
+            height: "4px"
+          }}>
+            <Col>
+              <Link to="#">
+                {title} {" "}
+                {/*<FontAwesomeIcon icon={["fab", "twitter"]} />*/}
+              </Link>
+            </Col>
+            <Col style={{
+              textAlign: "right"
+            }}>
+              <Link to="https://ieee-edoc.org/mailinglist.html">
+                <small>To keep informed about EDOC, <u>subscribe to our mailing list</u></small>
+              </Link>
+            </Col>
+          </Row>
           <Link to="https://www.gatsbyjs.com/">
             <br />
             {/*<strong>By {author}</strong>*/}
-            <strong>Powered by @<u>Gatsby</u></strong><br/>
+            <strong>Powered by @<u>Gatsby</u></strong><br />
           </Link>
           <Link to="https://www.destinationgoldcoast.com/">
             <small>Images courtesy of <u>Gold Coast Tourism</u></small>
           </Link>
+          <br />
         </p>
       </div>
     </div>
